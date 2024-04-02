1fd5ca027b98c9740e9d4e17be00c8090622ccbf
MemoG55 <115684918+MemoG55@users.noreply.github.com>
2024-04-02 22:02:06 +0900

    practice

---
+++ Hacking2_Affine/Hacking2_Affine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacking2_Affine
+{//3, 6 MYNAMEIS
+ //3, 2 ILOVEMYSCHOOL
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String strOriginal = "MYNAMEIS";//평문
+            int i;
+            int ascii;
+            char[] cipher = new char[strOriginal.Length];
+            for (i = 0; i < strOriginal.Length; i++)
+            {
+                ascii = Convert.ToInt32(strOriginal[i]);
+                ascii = (ascii - 65) * 3 + 6; //암호키
+                if (ascii > 25)
+                {
+                    ascii = ascii % 26;
+                }
+                ascii = ascii + 65;
+                cipher[i] = Convert.ToChar(ascii);
+            }
+            Console.WriteLine(cipher);
+        }
+    }
+}
